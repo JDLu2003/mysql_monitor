04ca4006b26003a7c8786aff656fbe0662054ee5
JDLu2003 <1277319667@qq.com>
2025-07-25 15:33:40 +0800

    fix: unuseable feat

--- mysql_monitor.c
+++ mysql_monitor.c
@@ -1,6 +1,5 @@
 // SPDX-License-Identifier: (LGPL-2.1 OR BSD-2-Clause)
 // Copyright (c) 2024 jdLu
-#include <argp.h>
 #include <signal.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -24,52 +23,15 @@ struct event {
 	char filename[MAX_FILENAME_LEN];
 };
 
-// Command-line arguments
-static struct env {
-	char *output_file;
-	bool verbose;
-} env = { .output_file = NULL, .verbose = false };
-
-// Argument parser
-const char *argp_program_version = "mysql_monitor 0.1";
-const char *argp_program_bug_address = "https://github.com/iovisor/bcc/tree/master/libbpf-tools";
-static const char doc[] =
-	"Monitor MySQL page cache readahead I/O using eBPF.\n"
-	"\n"
-	"USAGE: ./mysql_monitor [-o FILE]\n";
-
-static const struct argp_option opts[] = {
-	{ "output", 'o', "FILE", 0, "Path to a CSV file to save the output" },
-	{ "verbose", 'v', 0, 0, "Verbose debug output" },
-	{ 0 },
-};
-
-static error_t parse_arg(int key, char *arg, struct argp_state *state)
-{
-	switch (key) {
-	case 'o':
-		env.output_file = arg;
-		break;
-	case 'v':
-		env.verbose = true;
-		break;
-	case ARGP_KEY_ARG:
-		argp_usage(state);
-		break;
-	default:
-		return ARGP_ERR_UNKNOWN;
-	}
-	return 0;
-}
-
 static volatile bool exiting = false;
 static FILE *output_f = NULL;
+static bool verbose = false;
 
 // Libbpf logging callback
 static int libbpf_print_fn(enum libbpf_print_level level, const char *format,
 			   va_list args)
 {
-	if (level == LIBBPF_DEBUG && !env.verbose)
+	if (level == LIBBPF_DEBUG && !verbose)
 		return 0;
 	return vfprintf(stderr, format, args);
 }
@@ -102,16 +64,38 @@ void handle_event(void *ctx, int cpu, void *data, __u32 data_sz)
 	}
 }
 
+static void print_usage(const char *prog)
+{
+	printf("USAGE: %s [-o FILE] [-v]\n", prog);
+	printf("  -o FILE  Path to a CSV file to save the output\n");
+	printf("  -v       Verbose debug output\n");
+	printf("  -h       Show this help message\n");
+}
+
 int main(int argc, char **argv)
 {
 	struct mysql_monitor_bpf *skel;
 	struct perf_buffer *pb = NULL;
-	int err;
+	char *output_file = NULL;
+	int err, opt;
 
-	// Parse command-line arguments
-	err = argp_parse(&argp, argc, argv, 0, NULL, NULL);
-	if (err)
-		return err;
+	// Parse command-line arguments using getopt
+	while ((opt = getopt(argc, argv, "o:vh")) != -1) {
+		switch (opt) {
+		case 'o':
+			output_file = optarg;
+			break;
+		case 'v':
+			verbose = true;
+			break;
+		case 'h':
+			print_usage(argv[0]);
+			return 0;
+		default:
+			print_usage(argv[0]);
+			return 1;
+		}
+	}
 
 	// Set up libbpf logging
 	libbpf_set_print(libbpf_print_fn);
@@ -140,11 +124,11 @@ int main(int argc, char **argv)
 	signal(SIGTERM, sig_handler);
 
 	// Open output file if specified
-	if (env.output_file) {
-		output_f = fopen(env.output_file, "w");
+	if (output_file) {
+		output_f = fopen(output_file, "w");
 		if (!output_f) {
 			fprintf(stderr, "Error: Failed to open output file %s\n",
-				env.output_file);
+				output_file);
 			err = 1;
 			goto cleanup;
 		}
@@ -152,7 +136,7 @@ int main(int argc, char **argv)
 		fprintf(output_f,
 			"Timestamp,PID,Command,File,Pages,Offset\n");
 		printf("Capturing data... Saving to %s. Press Ctrl+C to stop.\n",
-		       env.output_file);
+		       output_file);
 	} else {
 		// Print table header
 		printf("%-20s %-7s %-16s %-30s %-7s %-10s\n", "TIMESTAMP",
